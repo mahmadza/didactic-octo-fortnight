3ecfd7a5c46328e2aa1fc30426a5e6e2cdfde632
Muhammad Zabidi <mamduh.zabidi@gmail.com>
2020-09-16 20:49:25 +0800

    Create blast+_setup_160920.sh

---
+++ blast+_setup_160920.sh
@@ -0,0 +1,17 @@
+
+
+
+
+#download dm3 sequences
+wget https://hgdownload.soe.ucsc.edu/goldenPath/dm3/bigZips/dm3.fa.gz
+#expand
+gunzip dm3.fa.gz
+
+#create index
+cd ~/Applications/ncbi-blast-2.10.1+/blastdb
+../bin/makeblastdb -in ../sequences/dm3.fa -dbtype nucl
+
+
+
+
+####
