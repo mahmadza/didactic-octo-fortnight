06e9863e4ef2681bdd6d7f984a8631ab4a5c1afc
Muhammad Zabidi <mamduh.zabidi@gmail.com>
2020-09-22 22:48:03 +0800

    Update blast+_setup_160920.sh

--- blast+_setup_160920.sh
+++ blast+_setup_160920.sh
@@ -2,15 +2,17 @@
 
 
 
+cd ~/Applications/ncbi-blast-2.10.1+
+
 #download dm3 sequences
+cd sequences
 wget https://hgdownload.soe.ucsc.edu/goldenPath/dm3/bigZips/dm3.fa.gz
 #expand
 gunzip dm3.fa.gz
 
-#create index
-cd ~/Applications/ncbi-blast-2.10.1+/blastdb
-../bin/makeblastdb -in ../sequences/dm3.fa -dbtype nucl -out dm3
-cd ..
+#build  index
+cd ~/Applications/ncbi-blast-2.10.1+
+./bin/makeblastdb -in sequences/dm3.fa -dbtype nucl -out blastdb/dm3
 
 
 
