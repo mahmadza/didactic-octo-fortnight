5c95dc8658610111c5474f629a72e99cce57f037
Muhammad Zabidi <mamduh.zabidi@gmail.com>
2020-10-04 11:36:49 +0800

    repaired downloading sequences

--- grabSequences_220920.sh
+++ grabSequences_220920.sh
@@ -9,19 +9,35 @@ export BLASTDB=~/Applications/ncbi-blast-2.10.1+/blastdb
 width=50
 
 #download eTSS sequences
-cat zfh1_genome-wide_eTSS.txt | while read l; do
+#cat zfh1_genome-wide_eTSS.txt | while read l; do
+#  chr=$(echo $l | awk '{print $1}')
+#  start=$(echo $l | awk -v p=$width '{print $2+1-p}')
+#  end=$(echo $l | awk -v p=$width '{print $3+p}')
+#  tss=$(echo $l | awk '{print $4}')
+#  strand=$(echo $l | awk '($5=="+"){s="plus"}($5=="-"){s="minus"} END{print s}')
+#
+#  seq=$(~/Applications/ncbi-blast-2.10.1+/bin/blastdbcmd -db dm3 \
+#  -entry $chr -range ${start}-${end} -strand ${strand} -outfmt %s)
+#
+#  echo -en $chr"\t"$start"\t"$end"\t"$tss"\t"$(echo $l | awk '{print $5}')"\t"$seq"\n"
+#
+#done | gzip -c > zfh1_genome-wide_eTSS_seq.txt.gz
+
+awk 'NR>1' plus_1_pos.txt | while read l; do
   chr=$(echo $l | awk '{print $1}')
   start=$(echo $l | awk -v p=$width '{print $2+1-p}')
   end=$(echo $l | awk -v p=$width '{print $3+p}')
   tss=$(echo $l | awk '{print $4}')
   strand=$(echo $l | awk '($5=="+"){s="plus"}($5=="-"){s="minus"} END{print s}')
+  respnss=$(echo $l | awk '{print $8}')
 
   seq=$(~/Applications/ncbi-blast-2.10.1+/bin/blastdbcmd -db dm3 \
   -entry $chr -range ${start}-${end} -strand ${strand} -outfmt %s)
 
-  echo -en $chr"\t"$start"\t"$end"\t"$tss"\t"$(echo $l | awk '{print $5}')"\t"$seq"\n"
+  echo -en $chr"\t"$start"\t"$end"\t"$tss"\t"$respnss"\t"$(echo $l | awk '{print $5}')"\t"$seq"\n"
 
 done | gzip -c > zfh1_genome-wide_eTSS_seq.txt.gz
+
 
 
 #download other sequences
