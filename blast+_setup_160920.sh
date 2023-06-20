8959b81cde6f3dfeb249b31ccdfd215c1a9f63e5
Muhammad Zabidi <mamduh.zabidi@gmail.com>
2023-06-20 23:14:40 +0800

    rearranged files

--- blast+_setup_160920.sh
+++ blast+_setup_160920.sh
@@ -1,6 +1,7 @@
 
 
-
+# This is to set up NCBI Blast application
+# The application is used to download the DNA sequences
 
 cd ~/Applications/ncbi-blast-2.10.1+
 
@@ -10,7 +11,7 @@ wget https://hgdownload.soe.ucsc.edu/goldenPath/dm3/bigZips/dm3.fa.gz
 #expand
 gunzip dm3.fa.gz
 
-#build index
+#build index for dm3
 cd ~/Applications/ncbi-blast-2.10.1+
 ./bin/makeblastdb -in sequences/dm3.fa -dbtype nucl -out blastdb/dm3 -parse_seqids
 
--- install_env.sh
+++ install_env.sh
@@ -1,2 +1,5 @@
+
+# Environment installation instructions
+
 conda create -n didactic-octo-fortnight python=3.9
 conda activate didactic-octo-fortnight
